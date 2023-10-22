bf9b396040e7a770a43f8f385136984ae44c6b20
Ashis <amaity@digisprint.com>
2023-10-22 19:37:33 +0530

    initial commit

---
+++ Basic/index.ts
@@ -0,0 +1,5 @@
+// What is Typescript?
+// Typescript is a javascript super set. It is a language building on top of javascript.
+// It adds new features and advantages to javascript.
+
+// One disadvantage is typescript can't be run in brawser.
